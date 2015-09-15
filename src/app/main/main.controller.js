d9904965e8e07d615b11d00ca233cc09b0ecb1b7
jmanock <jonmanock@gmail.com>
2015-09-15 11:23:48 -0400

    fucking finally got things to show up when logged in, need to save the data now

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,36 +3,71 @@
 
   angular
     .module('thatOneThing')
+    .factory('Auth', Auth)
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController(toastr, FirebaseUrl, $scope, $firebaseObject) {
+  function Auth($firebaseAuth, FirebaseUrl){
+    var endingPoints = FirebaseUrl;
+    var userRef = new Firebase(endingPoints);
+    return $firebaseAuth(userRef);
+  }
+
+  function MainController(toastr, $scope, Auth) {
     var vm = this;
-    vm.login = login;
 
-    function login(){
-      vm.message = !vm.message;
-      var ref = new Firebase(FirebaseUrl);
-      ref.authWithOAuthPopup('facebook', function(error, authData){
-        if(!error){
-          toastr.info('Something went wrong but its right');
-          var user = ref.child('users').child(authData.uid);
-          user.update({
-            uid:authData.uid,
-            facebook:authData.facebook,
-            fullName:authData.facebook.displayName
+    $scope.login = function(authMethod){
+      Auth.$authWithOAuthPopup(authMethod).then(function(authData){
+        Auth.$onAuth(function(authData){
+          if(authData === null){
+            console.log('no log in yet');
+          }else{
+            console.log('Logged in as', authData.facebook);
+          }
+          $scope.authData = authData;
+        });
+      }).catch(function(error){
+        if(error.code === 'TRANSPORT_UNAVAILABLE'){
+          Auth.$authWithOAuthReRedirect(authMethod).then(function(authDat){
+            console.log('this should not show up');
           });
-
-          // console.log(user);
-          return user;
-           //console.log(authData.facebook);
-
-
         }else{
-
+          console.log(error);
         }
       });
-    }
+      // Auth.$onAuth(function(authData){
+      //   if(authData === null){
+      //     console.log('No log in yet');
+      //   }else{
+      //     console.log('Logged in as', authData);
+      //   }
+      //   $scope.authData = authData;
+      // });
+    };
+
+    // function login(){
+    //   vm.message = !vm.message;
+    //   var ref = new Firebase(FirebaseUrl);
+    //   ref.authWithOAuthPopup('facebook', function(error, authData){
+    //     if(!error){
+    //       toastr.info('Something went wrong but its right');
+    //       var user = ref.child('users').child(authData.uid);
+    //       user.update({
+    //         uid:authData.uid,
+    //         facebook:authData.facebook,
+    //         fullName:authData.facebook.displayName
+    //       });
+
+
+
+
+
+
+      //   }else{
+      //
+      //   }
+      // });
+    //}
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
