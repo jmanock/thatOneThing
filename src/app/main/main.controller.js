060bd260ff382c839f7569f33db43bda41e2434b
jmanock <jonmanock@gmail.com>
2015-09-15 20:17:53 -0400

    think i got everything to work but the if statments

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -17,13 +17,14 @@
       if(authData === null){
         return null;
       }
-      var user = fbUrl.child('user').child(authData.uid);
+      var user = fbUrl.child('users').child(authData.uid);
       user.update({
         uid:authData.uid,
         facebook:authData.facebook,
         fullName:authData.facebook.displayName
       });
       user = $firebaseObject(fbUrl.child('users').child(authData.uid));
+
       return user;
     }
     return{
@@ -37,87 +38,14 @@
       }
     };
   }
-  // function Auth($firebaseAuth, FirebaseUrl){
-  //   var endingPoints = FirebaseUrl;
-  //   var userRef = new Firebase(endingPoints);
-  //   return $firebaseAuth(userRef);
-  // }
 
   function MainController(toastr, Auth) {
     var vm = this;
-    this.login = Auth.login;
+    vm.login = Auth.login;
     Auth.onAuth(function(user){
       vm.user = user;
+      console.log(user);
     });
-    // $scope.login = function(authMethod){
-    //   Auth.$authWithOAuthPopup(authMethod).then(function(authData){
-    //     Auth.$onAuth(function(authData){
-    //       if(authData === null){
-    //         console.log('no log in yet');
-    //       }else{
-    //         console.log('Logged in as', authData.facebook);
-    //       }
-    //       $scope.authData = authData;
-    //     });
-    //   }).catch(function(error){
-    //     if(error.code === 'TRANSPORT_UNAVAILABLE'){
-    //       Auth.$authWithOAuthReRedirect(authMethod).then(function(authDat){
-    //         console.log('this should not show up');
-    //       });
-    //     }else{
-    //       console.log(error);
-    //     }
-    //   });
-    //   // Auth.$onAuth(function(authData){
-      //   if(authData === null){
-      //     console.log('No log in yet');
-      //   }else{
-      //     console.log('Logged in as', authData);
-      //   }
-      //   $scope.authData = authData;
-      // });
-  //  };
-
-    // function login(){
-    //   vm.message = !vm.message;
-    //   var ref = new Firebase(FirebaseUrl);
-    //   ref.authWithOAuthPopup('facebook', function(error, authData){
-    //     if(!error){
-    //       toastr.info('Something went wrong but its right');
-    //       var user = ref.child('users').child(authData.uid);
-    //       user.update({
-    //         uid:authData.uid,
-    //         facebook:authData.facebook,
-    //         fullName:authData.facebook.displayName
-    //       });
-
-
-
-
-
-
-      //   }else{
-      //
-      //   }
-      // });
-    //}
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1441820769124;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-
-
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
 
   }
 })();
