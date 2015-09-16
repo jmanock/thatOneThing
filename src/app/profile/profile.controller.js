ceab090a2cb5cfa9a74b0884f959d9ba9fa1c090
jmanock <jonmanock@gmail.com>
2015-09-16 06:17:51 -0400

    starting to look good

--- src/app/profile/profile.controller.js
+++ src/app/profile/profile.controller.js
@@ -18,8 +18,8 @@
     */
 
 
-    var players = [];
     $http.get('app/json/field.json').success(function(data){
+      var players = [];
       var tPlayers = data.Tournament.Players;
       tPlayers.forEach(function(x){
         var names = x.PlayerName;
@@ -28,8 +28,44 @@
         players.push({
           Name:fieldNames
         });
-        vm.field = players;
+
+      });
+      $http.get('app/json/fedexPoints.json').success(function(data){
+        var fedexCup = [];
+        var standings = data.standings;
+        standings.forEach(function(x){
+          var firstName = x.firstName;
+          var lastName = x.lastName;
+          var fullName = firstName +' '+lastName;
+          fedexCup.push({
+            Name:fullName
+          });
+        });
+        var rankings = [];
+        for(var i = 0; i<fedexCup.length; i++){
+          for(var x = 0; x<players.length; x++){
+            if(fedexCup[i].Name === players[x].Name){
+              rankings.push(fedexCup[i].Name);
+            }
+          }
+        }
+
+        var aPlayers = rankings.splice(0,19);
+        vm.aPlayers = aPlayers;
+        var bPlayers = rankings.splice(20,45);
+        vm.bPlayers = bPlayers;
+        var cPlayers = rankings;
+        vm.cPlayers = cPlayers;
       });
     });
+
+    
+    vm.profile = 1;
+    vm.setTab = function(tabId){
+      vm.profile = tabId;
+    };
+    vm.isSet = function(tabId){
+      return vm.profile === tabId;
+    };
   }
 })();
