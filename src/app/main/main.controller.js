0f9e39a2d6a223c406be40c8485bfa2abd29f08f
jmanock <jonmanock@gmail.com>
2015-09-16 02:47:54 -0400

    not sure if this is the best way to do it but it works

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,58 +3,33 @@
 
   angular
     .module('thatOneThing')
-    .factory('fbUrl', fbUrl)
     .factory('Auth', Auth)
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function fbUrl(FirebaseUrl){
-    return new Firebase(FirebaseUrl);
+  function Auth(FirebaseUrl, $firebaseAuth){
+    var auth = new Firebase(FirebaseUrl);
+    return $firebaseAuth(auth);
   }
-  function Auth(fbUrl, $firebaseAuth, $firebaseObject){
-    var auth = $firebaseAuth(fbUrl);
-    function updateUser(authData){
-      if(authData === null){
-        return null;
-      }
-      var user = fbUrl.child('users').child(authData.uid);
-      user.update({
-        uid:authData.uid,
-        facebook:authData.facebook,
-        fullName:authData.facebook.displayName
-      });
-      user = $firebaseObject(fbUrl.child('users').child(authData.uid));
-      return user;
-    }
-    return{
-      onAuth:function(cb){
-        auth.$onAuth(function(data){
-          cb(updateUser(data));
-        });
-      },
-      login:function(){
-        return auth.$authWithOAuthPopup('facebook');
-      }
-    };
-  }
-
   function MainController(toastr, Auth) {
     var vm = this;
-    // This should not do anything till i hit the fucking button.
-    //vm.login = Auth.login;
-    // Auth.onAuth(function(user){
-    //
-    //   vm.user = user;
-    //
-    // });
-
-    vm.login = fblogin;
-    function fblogin(){
-      vm.login = Auth.login;
-      Auth.onAuth(function(user){
-        
-        vm.user = user;
+    vm.login = login;
+    vm.logout = logout;
+    Auth.$onAuth(function(authData){
+      vm.authData = authData;
+      if(authData){
+        console.log(authData);
+      }else{
+        console.log(authData);
+      }
+    });
+    function login(){
+      Auth.$authWithOAuthPopup('facebook').catch(function(error){
+        console.error('Error authenticating with facebook', error);
       });
+    }
+    function logout(){
+      Auth.$unauth();
     }
   }
 })();
