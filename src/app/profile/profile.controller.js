884063302645a7c84092b578de12a15810df7c3b
jmanock <jonmanock@gmail.com>
2015-09-16 04:41:41 -0400

    got the players up and running

--- src/app/profile/profile.controller.js
+++ src/app/profile/profile.controller.js
@@ -4,7 +4,23 @@
   .module('thatOneThing')
   .controller('ProfileController', ProfileController);
 
-  function ProfileController(){
-    
+  function ProfileController($http, $stateParams, $firebaseArray, FirebaseUrl){
+    var vm = this;
+    // vm.currentUser = $firebaseArray(FirebaseUrl.child('user').child($stateParams.id));
+    // console.log(vm.currentUser);
+
+    var players = [];
+    $http.get('app/field.json').success(function(data){
+      var tPlayers = data.Tournament.Players;
+      tPlayers.forEach(function(x){
+        var names = x.PlayerName;
+        var parts = names.split(', ');
+        var fieldNames = parts[1]+' '+parts[0];
+        players.push({
+          Name:fieldNames
+        });
+        vm.field = players;
+      });
+    });
   }
 })();
