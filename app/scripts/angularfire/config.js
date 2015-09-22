618128a3d4879707c076109f46d733bb0266d37e
jmanock <jonmanock@gmail.com>
2015-09-22 15:28:51 -0400

    removing old files

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://reditclone.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','facebook'])
-
-  .constant('loginRedirectPath', '/login');
