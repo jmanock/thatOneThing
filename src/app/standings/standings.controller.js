17c5c3b5b5525949c6387708a0b7d632d5d6598b
jmanock <jonmanock@gmail.com>
2015-09-23 11:57:55 -0400

    somethings show up others spit in my face, not sure where I am going with this, still have to get the points and everything from the leaderboard

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -9,26 +9,8 @@
     var ref = new Firebase(FirebaseUrl);
     init();
     function init(){
-      var something =[];
-      var teams = $firebaseArray(ref.child('userTeam'));
-      vm.users = teams;
-      teams.$loaded().then(function(data){
-        data.forEach(function(k){
-          var user = k.$id
-
-          var team = $firebaseArray(ref.child('userTeam').child(user).child('Team'));
-          //vm.users = team;
-          team.$loaded().then(function(data){
-            //something.User = user;
-            data.forEach(function(x){
-              var players = x.$id;
-              something.push(players);
-            });
-            console.log(something);
-          });
-
-        });
-      });
+      var Users = $firebaseObject(ref.child('userTeam'));
+      vm.users = Users;
     }
   }
 })();
