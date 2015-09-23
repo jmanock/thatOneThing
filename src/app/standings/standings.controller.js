b03dff7cbb5ad57e7cde64f271f7e150a09eb320
jmanock <jonmanock@gmail.com>
2015-09-23 17:49:41 -0400

    have to find a better way to add and show data

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -10,13 +10,6 @@
     init();
     function init(){
       var leaderboard = $firebaseArray(ref.child('leaderboard'));
-      var teams = $firebaseObject(ref.child('userTeam'));
-      teams.$loaded().then(function(data){
-        data.forEach(function(j){
-          var team = j.Team;
-          console.log(team);
-        });
-      });
       vm.leaderboard = leaderboard;
       leaderboard.$loaded().then(function(data){
         data.forEach(function(x){
