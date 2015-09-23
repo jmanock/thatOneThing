6ca7a1ff38a2d8ae4250161d5e07853f3a438aed
jmanock <jonmanock@gmail.com>
2015-09-23 10:59:25 -0400

    console prints what I want but getting dups error when i convert it

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -4,7 +4,28 @@
   .module('thatOneThing')
   .controller('StandingsController', StandingsController);
 
-  function StandingsController(){
+  function StandingsController(FirebaseUrl, $firebaseArray, $firebaseObject){
+    var vm = this;
+    var ref = new Firebase(FirebaseUrl);
+    init();
+    function init(){
 
+      var teams = $firebaseArray(ref.child('userTeam'));
+      teams.$loaded().then(function(data){
+        data.forEach(function(k){
+          var user = k.$id
+
+          var team = $firebaseArray(ref.child('userTeam').child(user).child('Team'));
+          team.$loaded().then(function(data){
+
+            //vm.users = user;
+            data.forEach(function(x){
+              var players = x.$id;
+              vm.users = players;
+            });
+          });
+        });
+      });
+    }
   }
 })();
