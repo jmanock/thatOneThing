358315b6a5f46f6fe8bf1bd3a49004786b7863e3
jmanock <jonmanock@gmail.com>
2015-09-23 17:36:08 -0400

    might have to rethink how i add data to fb, this doesnt seem like its going to work to well

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -9,8 +9,21 @@
     var ref = new Firebase(FirebaseUrl);
     init();
     function init(){
-      var Users = $firebaseObject(ref.child('userTeam'));
-      vm.users = Users;
+      var leaderboard = $firebaseArray(ref.child('leaderboard'));
+      var teams = $firebaseObject(ref.child('userTeam'));
+      teams.$loaded().then(function(data){
+        data.forEach(function(j){
+          var team = j.Team;
+          console.log(team);
+        });
+      });
+      vm.leaderboard = leaderboard;
+      leaderboard.$loaded().then(function(data){
+        data.forEach(function(x){
+          //console.log(x.Name);
+        });
+      });
+
     }
   }
 })();
