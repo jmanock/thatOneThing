fa11f5425c07326f40e7e12a289c9f4a3adf9fdb
jmanock <jonmanock@gmail.com>
2015-09-24 03:04:08 -0400

    this might end up being a good one

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -6,15 +6,22 @@
 
   function StandingsController($scope, FirebaseUrl, $firebaseArray, $firebaseObject){
     var vm = this;
-  init();
-  function init(){
-    var names = new Firebase('https://reditclone.firebaseio.com/userTeam');
-    names.on('child_added', function(snapshot){
-      var data = snapshot.val();
-      var name = data.name;
-      var score = data.score;
-      console.log(name);
-    });
-  }
+    var fireRef = new Firebase('https://reditclone.firebaseio.com/toDo/');
+    $scope.todos = $firebaseArray(fireRef);
+    $scope.newTodo = '';
+    $scope.addTodo = function(){
+      var newTodo = $scope.newTodo.trim();
+      if(!newTodo.length){
+        return;
+      }
+        $scope.todos.$add({
+          title:newTodo,
+          completed:false
+        });
+        $scope.newTodo = '';
+    };
+    $scope.removeTodo = function(todo){
+      $scope.todos.$remove(todo);
+    };
   }
 })();
