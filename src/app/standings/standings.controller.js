1889f3ddadd9a23d661154cdfba56edbded1af01
jmanock <jonmanock@gmail.com>
2015-09-24 02:29:24 -0400

    more examples still not able to use any of them

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -4,34 +4,17 @@
   .module('thatOneThing')
   .controller('StandingsController', StandingsController);
 
-  function StandingsController(FirebaseUrl, $firebaseArray, $firebaseObject){
+  function StandingsController($scope, FirebaseUrl, $firebaseArray, $firebaseObject){
     var vm = this;
-    var ref = new Firebase(FirebaseUrl);
-    init();
-    function init(){
-      var leaderboard = $firebaseArray(ref.child('leaderboard'));
-      vm.leaderboard = leaderboard;
-      leaderboard.$loaded().then(function(data){
-        data.forEach(function(x){
-          //console.log(x.Name);
-        });
-      });
-      var userTeam = $firebaseObject(ref.child('userTeam'));
-      userTeam.$loaded().then(function(data){
-        data.forEach(function(a){
-          var name = a.name;
-          var team = a.Team;
-
-          var teamPlayers = $firebaseObject(ref.child('userTeam').child(name).child('Team'));
-          teamPlayers.$loaded().then(function(ddata){
-            console.log(name);
-            ddata.forEach(function(x){
-              console.log(x.name);
-            });
-          });
-        });
-      });
-
-    }
+  init();
+  function init(){
+    var names = new Firebase('https://reditclone.firebaseio.com/userTeam');
+    names.on('child_added', function(snapshot){
+      var data = snapshot.val();
+      var name = data.name;
+      var score = data.score;
+      console.log(name);
+    });
+  }
   }
 })();
