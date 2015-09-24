2bbf289ef62a63768313f0df4b9f3b616a79b5c8
jmanock <jonmanock@gmail.com>
2015-09-24 13:52:53 -0400

    this might be a good one

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -6,22 +6,23 @@
 
   function StandingsController($scope, FirebaseUrl, $firebaseArray, $firebaseObject){
     var vm = this;
-    var fireRef = new Firebase('https://reditclone.firebaseio.com/toDo/');
-    $scope.todos = $firebaseArray(fireRef);
-    $scope.newTodo = '';
-    $scope.addTodo = function(){
-      var newTodo = $scope.newTodo.trim();
-      if(!newTodo.length){
-        return;
-      }
-        $scope.todos.$add({
-          title:newTodo,
-          completed:false
-        });
-        $scope.newTodo = '';
-    };
-    $scope.removeTodo = function(todo){
-      $scope.todos.$remove(todo);
-    };
+    var fireRef = new Firebase('https://reditclone.firebaseio.com/userTeam/');
+    // $scope.todos = $firebaseArray(fireRef);
+    // $scope.newTodo = '';
+    // $scope.addTodo = function(){
+    //   var newTodo = $scope.newTodo.trim();
+    //   if(!newTodo.length){
+    //     return;
+    //   }
+    //     $scope.todos.$add({
+    //       title:newTodo,
+    //       completed:false
+    //     });
+    //     $scope.newTodo = '';
+    // };
+    // $scope.removeTodo = function(todo){
+    //   $scope.todos.$remove(todo);
+    // };
+    $scope.teams = $firebaseArray(fireRef);
   }
 })();
