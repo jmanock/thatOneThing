0a049981b33b3cdf732e6ee8ebf60d48b3eac79b
jmanock <jonmanock@gmail.com>
2015-09-28 13:43:31 -0400

    this might work

--- src/app/standings/standings.controller.js
+++ src/app/standings/standings.controller.js
@@ -2,36 +2,9 @@
   'use strict';
   angular
   .module('thatOneThing')
-  .filter('something', something)
+
   .controller('StandingsController', StandingsController);
 
-  function something($firebaseArray){
-    return function(input){
-      var ssomething = [];
-      var knew = [];
-      angular.forEach(input, function(x){
-        ssomething.push(x.Name);
-      });
-      var ref = new Firebase('https://reditclone.firebaseio.com/leaderboard');
-      var fRef = $firebaseArray(ref);
-      fRef.$loaded().then(function(data){
-        data.forEach(function(a){
-          var kname = a.Name;
-          ssomething.forEach(function(b){
-            if(b === kname){
-              knew.push({
-                Name:b,
-                Points:a.Total
-              });
-            }
-          });
-          
-        });
-
-      });
-
-    };
-  }
   function StandingsController($scope, FirebaseUrl, $firebaseArray, $firebaseObject){
     var vm = this;
     var fireRef = new Firebase('https://reditclone.firebaseio.com/userTeam/');
@@ -56,6 +29,12 @@
       $scope.teams = $firebaseArray(fireRef);
 
     }
-
+    $scope.getStuff = function(x){
+      var team = x.Team;
+      angular.forEach(team, function(z){
+        var name = z.Name;
+        console.log(name);
+      });
+    };
   }
 })();
